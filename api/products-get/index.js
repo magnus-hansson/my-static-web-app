5b807dcb0cd02cc5862060875c4c1a5e68c875ae
magnus-hansson <hanssmag+git@gmail.com>
2020-05-24 19:47:49 +0200

    wo binding

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,10 +1,26 @@
+const CosmosClient = require("@azure/cosmos").CosmosClient;
 const data = require("../shared/product-data");
 
 module.exports = async function (context, req) {
   try {
-    const products = context.bindings.inputDocumentIn;
+    let endpoint = process.env["endpoint"];
+    let key = process.env["key"];
+    const client = new CosmosClient({ endpoint, key });
 
-    context.res.status(200).json(products);
+    const database = client.database("mydb");
+    const container = database.container("mycol");
+
+    const querySpec = {
+      query: "SELECT * from c",
+    };
+
+    const { resources: items } = await container.items
+      .query(querySpec)
+      .fetchAll();
+
+    //const products = context.bindings.inputDocumentIn;
+    //const products = data.getProducts();
+    context.res.status(200).json(items);
   } catch (error) {
     context.res.status(500).send(error);
   }
