bb3bd07d2eabb8fada14f14b16db789a52d39baa
magnus-hansson <hanssmag+git@gmail.com>
2020-05-24 19:52:30 +0200

    refactor

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,15 +1,13 @@
 const CosmosClient = require("@azure/cosmos").CosmosClient;
-const data = require("../shared/product-data");
+let endpoint = process.env["endpoint"];
+let key = process.env["key"];
+const client = new CosmosClient({ endpoint, key });
+
+const database = client.database("mydb");
+const container = database.container("mycol");
 
 module.exports = async function (context, req) {
   try {
-    let endpoint = process.env["endpoint"];
-    let key = process.env["key"];
-    const client = new CosmosClient({ endpoint, key });
-
-    const database = client.database("mydb");
-    const container = database.container("mycol");
-
     const querySpec = {
       query: "SELECT * from c",
     };
@@ -18,8 +16,6 @@ module.exports = async function (context, req) {
       .query(querySpec)
       .fetchAll();
 
-    //const products = context.bindings.inputDocumentIn;
-    //const products = data.getProducts();
     context.res.status(200).json(items);
   } catch (error) {
     context.res.status(500).send(error);
